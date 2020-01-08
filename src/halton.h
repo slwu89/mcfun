04d9950efa223ddf0e2f7d5ddd05bb131178f623
slwu89 <slwu89@berkeley.edu>
2020-01-07 20:36:46 -0800

    list of primes

---
+++ src/halton.h
@@ -0,0 +1,20 @@
+/* --------------------------------------------------------------------------------
+#
+#   mcfun: fun with (Randomized-)(quasi-) Monte Carlo things
+#   Halton Algorithm
+#   C by Sean Wu (slwu89@berkeley.edu) adapted from Art B. Owen (https://statweb.stanford.edu/~owen/code/rhalton.R)
+#   January 2020
+#
+-------------------------------------------------------------------------------- */
+
+#ifndef HALTON_H
+#define HALTON_H
+
+#include <stdio.h>
+
+#include <R.h>
+#include <Rmath.h>
+
+
+
+#endif
