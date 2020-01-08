f0b3a1b4ff400f42fb3d1f05a4a471f607c5ea86
slwu89 <slwu89@berkeley.edu>
2020-01-07 20:24:52 -0800

    empty R pkg

---
+++ R/mcfun-C.R
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------------- #
+#
+#   mcfun: fun with (Randomized-)(quasi-) Monte Carlo things
+#   .Call interface to C functions
+#   Sean Wu (slwu89@berkeley.edu)
+#   January 2020
+#
+# -------------------------------------------------------------------------------- #
